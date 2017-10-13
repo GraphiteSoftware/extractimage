271f794dcf9807683172f92f09104ed3f3d786bb
ericstevens <eric@ericstevens.ca>
2017-10-13 10:52:14 -0400

    Added parse build prop base version

---
+++ parsebuildprop.py
@@ -0,0 +1,119 @@
+import json
+import sys
+from optparse import OptionParser
+import ericbase as eb
+import os.path
+import re
+from urllib.request import urlretrieve
+
+
+# define global variables
+# options as globals
+verbose = False
+debug = False
+test = False
+DEBUG = '[DEBUG] '
+VERBOSE = '[STATUS] '
+root, data, images, inputfile, outputfile = None, None, None, None, None
+
+def main():
+    """main processing loop"""
+    global verbose, debug, test, root, data, images, inputfile, outputfile
+    image_dict = {}
+    verbose, debug, test, root, images, outputfile = processargs()
+    if test:
+        print(VERBOSE, "Running in Test Mode")
+    rw = ReadWrite(root, images, outputfile)
+    if debug:
+        print(str(rw))
+    print(verbose, debug, test, rw.root_path, rw.url_list_path, rw.image_path, rw.input_json, rw.output_json)
+    for line in rw:
+        print(line)
+
+        
+class ReadWrite:
+    def __init__(self, rootpath: str, imagepath: str, outfile: str):
+        if rootpath is None:
+            self.root_path = '/Volumes/passport/test'
+        else:
+            self.root_path = rootpath
+        if imagepath is None:
+            self.image_path = 'sys'
+        else:
+            self.image_path = imagepath
+        if outfile is None:
+            self.output_json = 'imageprop.json'
+        else:
+            self.output_json = outfile
+        self.buildprop_file = 'build.prop'
+        # check that the path is valid and exists
+        if not os.path.exists(os.path.join(self.root_path, self.image_path)):
+            eb.printerror("Image directory does not exist or is not mounted: [path: " + os.path.join(self.root_path, self.image_path) + "]")
+        if not os.path.exists(os.path.join(self.root_path, self.image_path, self.buildprop_file)):
+            eb.printerror("Build properties file does not exist: [path: " + os.path.join(self.root_path, self.image_path, self.buildprop_file) + "]")
+
+    def __str__(self) -> str:
+        return "Input file is: " + os.path.join(self.root_path,
+                                                self.image_path,
+                                                self.buildprop_file) + \
+               "\nOutput file is: " + os.path.join(self.root_path,
+                                                   self.output_json + \
+                "\nImage path is: " + os.path.join(self.root_path,
+                                                 self.image_path) )
+
+    def readinput(self):
+        """read model and download url from json file"""
+        global verbose, debug, test
+        bp_file = os.path.join(self.root_path, self.image_path, self.buildprop_file)
+        bp_fh = open(bp_file, "r")
+        data = bp_fh.readlines()
+        json_fh.close()
+        if debug:
+            print(DEBUG + "Got json file")
+        return data
+
+    def writeoutput(self, idout: dict):
+        """write the image list dictionary to the json file"""
+        global verbose, debug, test
+        if debug:
+            print("{}{}".format(DEBUG, idout))
+        json_file = os.path.join(self.root_path, self.output_json)
+        json_fh = open(json_file, "w")
+        json.dump(idout, json_fh)
+        json_fh.close()
+
+
+def processargs():
+    """process arguments and options"""
+    usagemsg = "This program reads a json file that been output from the extract program and has a list of\
+     image urls and associated data. Then the images are downloaded, unzipped, and ripped to extract the build\
+      properties and write those to a JSON file"
+
+    parser = OptionParser(usagemsg)
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
+                      help="Print out helpful information during processing")
+    parser.add_option("-d", "--debug", dest="debug", action="store_true", default=False,
+                      help="Print out debug messages during processing")
+    parser.add_option("-t", "--test", dest="test", action="store_true", default=False,
+                      help="Use test file instead of full file list")
+    parser.add_option("-r", "--root", dest="rootpath", default=None,
+                      help="Root path to use for files and images", metavar="ROOTPATH")
+    parser.add_option("-i", "--image", dest="imagepath", default=None,
+                      help="Path to use for the image", metavar="IMAGEPATH")
+    parser.add_option("-o", "--output", dest="outputfile", default=None,
+                      help="JSON file to write the build.props data to", metavar="OUTPUTFILE")
+    (options, args) = parser.parse_args()
+
+    # required options checks
+    if options.debug:
+        options.verbose = True
+    return options.verbose, \
+           options.debug, \
+           options.test, \
+           options.rootpath, \
+           options.imagepath, \
+           options.outputfile
+
+
+if __name__ == '__main__':
+    main()
