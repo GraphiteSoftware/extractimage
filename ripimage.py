7f048062cbe791c95f6aae2d9b1a269f4a2c70af
ericstevens <eric@ericstevens.ca>
2017-10-13 10:38:16 -0400

    Added sdat2img.py

--- ripimage.py
+++ ripimage.py
@@ -1,11 +1,11 @@
-from bs4 import BeautifulSoup
-import httplib2
 import json
 import sys
 from optparse import OptionParser
 import ericbase as eb
 import os.path
 import re
+from urllib.request import urlretrieve
+
 
 # define global variables
 # options as globals
@@ -14,17 +14,47 @@ debug = False
 test = False
 DEBUG = '[DEBUG] '
 VERBOSE = '[STATUS] '
-
+root, data, images, inputfile, outputfile = None, None, None, None, None
 
 def main():
     """main processing loop"""
-    global verbose, debug, test
+    global verbose, debug, test, root, data, images, inputfile, outputfile
     image_dict = {}
     verbose, debug, test, root, data, images, inputfile, outputfile = processargs()
+    if test:
+        print(VERBOSE, "Running in Test Mode")
+        inputfile = 'urllist_test.json'
+        root = '.'
+        data = ''
+        images = ''
     rw = ReadWrite(root, data, images, inputfile, outputfile)
-    for line in rw.readinput():
-        image_dict[line['name']] = processline(line['pid'], line['name'])
-    rw.writeoutput(image_dict)
+    if debug:
+        print(str(rw))
+    d = rw.readinput()
+    print(verbose, debug, test, rw.root_path, rw.url_list_path, rw.image_path, rw.input_json, rw.output_json)
+    for line in d:
+        model = d[line]['name']
+        for i in d[line]['images']:
+            file_name = extractgroup(re.search(r"http:\/\/.*\/(.*)", i['image']))
+            if debug:
+                print(rw.root_path, rw.image_path, file_name)
+            file_path = os.path.join(rw.root_path, rw.image_path, file_name)
+            dl_message = "Downloading: " + i['image'] + " (" + model + ", " + i['region'] + ", " + i['channel'] +") to [" + file_path + "]"
+            print(dl_message)
+            # urlretrieve(i['image'], file_path, reporthook)
+
+
+def reporthook(blocknum, blocksize, totalsize):
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(totalsize)), readsofar, totalsize)
+        sys.stderr.write(s)
+        if readsofar >= totalsize:  # near the end
+            sys.stderr.write("\n")
+    else:  # total size is unknown
+        sys.stderr.write("read %d\n" % (readsofar,))
 
 
 class ReadWrite:
@@ -42,21 +72,28 @@ class ReadWrite:
         else:
             self.url_list_path = datapath
         if infile is None:
-            self.input_json = 'xmlist.json'
+            self.input_json = 'urllist.json'
         else:
             self.input_json = infile
         if outfile is None:
-            self.output_json = 'urllist.json'
+            self.output_json = 'imageprop.json'
         else:
             self.output_json = outfile
+        # check that the path is valid and exists
+        if not os.path.exists(os.path.join(self.root_path, self.image_path)):
+            eb.printerror("Image directory does not exists or is not mounted: [path: " + os.path.join(self.root_path, self.image_path) + "]")
+        if not os.path.exists(os.path.join(self.root_path, self.url_list_path)):
+            eb.printerror("Image directory does not exists or is not mounted: [path: " + os.path.join(self.root_path, self.url_list_path) + "]")
 
     def __str__(self) -> str:
         return "Input file is: " + os.path.join(self.root_path,
                                                 self.url_list_path,
-                                                self.output_json) + \
+                                                self.input_json) + \
                "\nOutput file is: " + os.path.join(self.root_path,
                                                    self.url_list_path,
-                                                   self.output_json)
+                                                   self.output_json + \
+                "\nImage path is: " + os.path.join(self.root_path,
+                                                 self.image_path) )
 
     def readinput(self):
         """read model and download url from json file"""
@@ -94,71 +131,17 @@ def extractgroup(match):
     return match.group(1)
 
 
-def walker(soup, ld):
-    global global_id, china_id, processing, model_name, channel
-    global verbose, debug, test
-    if soup.name is not None:
-        for child in soup.children:
-            if child.name == 'span':
-                # print("<SPAN>: ", child.text, child.attrs)
-                if bool(re.search("Global$", child.text)):
-                    global_id = 'content_' + child.get('id')
-                    if debug:
-                        print("Found Global: " + child.text + " with id of: " + child.get('id'))
-                if bool(re.search("China", child.text)):
-                    china_id = 'content_' + child.get('id')
-                    if debug:
-                        print("Found China: " + child.text + " with id of: " + child.get('id'))
-
-            if child.name == 'div':
-                # print("<DIV>: ", child.text, child.attrs)
-                if 'id' in child.attrs:
-                    if child.get('id') == global_id:
-                        processing = "global"
-                    if child.get('id') == china_id:
-                        processing = "china"
-                if 'class' in child.attrs:
-                    for c in child.get('class'):
-                        if c == 'download_nv':
-                            channel = extractgroup(re.search(r"^(.*?) ", child.text))
-            if child.name == 'a':
-               if 'class' in child.attrs:
-                    # print("Found a class of:", child.get('class'))
-                    if 'btn_5' in child.get('class'):
-                        if verbose:
-                            print("Processing", channel, processing, model_name, "link of:", child.get('href'))
-                            temp_d = {'image': child.get('href'), 'channel': channel, 'region': processing}
-                            ld['images'].append(temp_d)
-            ld = walker(child, ld)
-    return ld
-
-
-
 def processline(pid: str, n: str):
     """process the line by reading the html and extracting the information"""
     global verbose, debug, test
-    url = "http://en.miui.com/download-" + pid + ".html"
-    if verbose:
-        print(VERBOSE + "Name: {} -> {}".format(n, url))
-    line_dict = dict(url=url, name=n, images=[])
-
-    http = httplib2.Http()
-    status, response = http.request(url)
-
-    soup = BeautifulSoup(response, 'html.parser')
-
-    line_dict = walker(soup, line_dict)
-    if debug:
-        print(line_dict)
-    return line_dict
+    return 0
 
 
 def processargs():
     """process arguments and options"""
-    usagemsg = "This program reads a json file that has the Xiaomi model list as pulled from the Xiaomi download site\n\
-     and extracts all of the image URLs from all of the download pages. The URLS are written to a json file. \n\
-    Usage is:\n\n\
-    python3 " + sys.argv[0] + " [options] where:"
+    usagemsg = "This program reads a json file that been output from the extract program and has a list of\
+     image urls and associated data. Then the images are downloaded, unzipped, and ripped to extract the build\
+      properties and write those to a JSON file"
 
     parser = OptionParser(usagemsg)
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
@@ -174,7 +157,7 @@ def processargs():
     parser.add_option("-i", "--images", dest="imagepath", default=None,
                       help="Path to use for images", metavar="IMAGEPATH")
     parser.add_option("-m", "--model", dest="modelfile", default=None,
-                      help="JSON file with model and URL data", metavar="MODELFILE")
+                      help="JSON file with image URL data", metavar="MODELFILE")
     parser.add_option("-o", "--output", dest="outputfile", default=None,
                       help="JSON file to write the download URL data to", metavar="OUTPUTFILE")
     (options, args) = parser.parse_args()
