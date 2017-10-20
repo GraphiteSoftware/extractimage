06d0a2076578bb3454b98d0b801ca1459a5c459d
ericstevens <eric@ericstevens.ca>
2017-10-20 12:19:31 -0400

    updated parseprops to read the directory and extract the props into a json output

--- parsebuildprop.py
+++ parsebuildprop.py
@@ -1,110 +1,126 @@
 import json
-import sys
 from optparse import OptionParser
 import ericbase as eb
 import os.path
-import re
-from urllib.request import urlretrieve
+import re, fnmatch
 
 # define global variables
 # options as globals
-verbose = False
-debug = False
-test = False
-DEBUG = '[DEBUG] '
-VERBOSE = '[STATUS] '
-root, data, images, outputfile = None, None, None, None
+DEBUG = '[DEBUG]'
+VERBOSE = '[STATUS]'
+WARNING = '[WARNING]'
+ERROR = '[ERROR]'
 re_datetime = r"(.*?)=(.*)"
-output_dict = {}
 
 
 def main():
     """main processing loop"""
-    global verbose, debug, test, root, images, outputfile, output_dict
-    verbose, debug, test, root, images, outputfile = processargs()
-    if test:
+    do = MyArgs()
+    do.processargs()
+    if Flags.test:
         print(VERBOSE, "Running in Test Mode")
-    rw = ReadWrite(root, images, outputfile)
-    d = rw.readinput()
-    if debug:
-        print(str(rw))
-        print(verbose, debug, test, rw.root_path, rw.image_path, rw.output_json)
-    for line in d:
-        if debug:
-            print(line.rstrip())
-        if line[0] == '#':
-            if debug:
-                print("Comment line")
+    if Flags.debug:
+        print(DEBUG, "Flags are:\n\tVerbose: {}\n\tDebug: {}\n\tTest: {}\n\tConfig File: {}\n\tConfig Settings: {}".format(
+            Flags.verbose, Flags.debug, Flags.test, Flags.config, Flags.configsettings))
+    output_dict = {}
+    rw = ReadWrite(Flags.configsettings['root'], Flags.configsettings['extractprops'], Flags.configsettings['output'])
+    listofprops = getfilelist(os.path.join(Flags.configsettings['root'], Flags.configsettings['extractprops']))
+    for propfile in listofprops:
+        line_dict = {}
+        if Flags.debug:
+            print(DEBUG, propfile.rstrip())
+        d = rw.readinput(propfile)
+        for line in d:
+            if line[0] == '#':
+                if Flags.test:
+                    print("Comment line")
+                else:
+                    continue
             else:
-                continue
+                prop = extractgroups(re.search(re_datetime, line))
+                if prop is not None:
+                    if Flags.test:
+                        print("Found {} = {}".format(prop[0], prop[1]))
+                    line_dict[prop[0]] = prop[1]
+        head, file = os.path.split(propfile)
+        imgfields = {}
+        if file != '':
+            imgfields = splitfilename(file)
         else:
-            prop = extractgroups(re.search(re_datetime, line))
-            if prop is not None:
-                if debug:
-                    print("Found {} = {}".format(prop[0], prop[1]))
-                output_dict[prop[0]] = prop[1]
+            if Flags.debug:
+                print(WARNING, "bad file name in", propfile)
+            imgfields = {"model": '', "region": '', "channel": '', "version": ''}
+        imgfields['props'] = line_dict
+        output_dict[file] = imgfields
     rw.writeoutput(output_dict)
 
 
+def splitfilename(f: str) -> dict:
+    fieldpattern = r"(.*?)(China|Global)(Stable|Developer)(.*?).build.prop"
+    fields = {}
+    fields['model'], fields['region'], fields['channel'], fields['version'] = extractgroups(re.search(fieldpattern, f))
+    return fields
 
     # app/Spaces*/Spaces*.apk
     # priv-app/SpacesManagerService/Spaces*.apk
 
 
+def getfilelist(filepath) -> list:
+    fl = []
+    patternprop = r"*build.prop"
+    if not os.path.isdir(filepath):
+        eb.printerror("Build Props directory does not exist or is not mounted: " + filepath)
+    else:
+        for file in os.listdir(filepath):
+            # print("Checking: ", file)
+            if fnmatch.fnmatch(file, patternprop):
+                # print("Matches property file")
+                fl.append(os.path.join(filepath, file))
+    return fl
+
+
 class ReadWrite:
-    def __init__(self, rootpath: str, imagepath: str, outfile: str):
+    def __init__(self, rootpath: str, proppath: str, outfile: str):
         if rootpath is None:
-            self.root_path = '/Volumes/passport/test'
+            self.root_path = '/Volumes/passport'
         else:
             self.root_path = rootpath
-        if imagepath is None:
-            self.image_path = 'sys'
+        if proppath is None:
+            self.image_path = 'extracted_props'
         else:
-            self.image_path = imagepath
+            self.image_path = proppath
         if outfile is None:
             self.output_json = 'imageprop.json'
         else:
             self.output_json = outfile
-        self.buildprop_file = 'build.prop'
         # check that the path is valid and exists
         if not os.path.exists(os.path.join(self.root_path, self.image_path)):
             eb.printerror(
-                "Image directory does not exist or is not mounted: [path: " + os.path.join(self.root_path,
-                                                                                           self.image_path) + "]")
-        if not os.path.exists(os.path.join(self.root_path, self.image_path, self.buildprop_file)):
-            eb.printerror(
-                "Build properties file does not exist: [path: " + os.path.join(self.root_path, self.image_path,
-                                                                               self.buildprop_file) + "]")
+                "Build Prop directory does not exist or is not mounted: [path: " + os.path.join(self.root_path,
+                                                                                                self.image_path) + "]")
 
     def __str__(self) -> str:
-        return "Input file is: " + os.path.join(self.root_path,
-                                                self.image_path,
-                                                self.buildprop_file) + \
-               "\nOutput file is: " + os.path.join(self.root_path,
-                                                   self.output_json +
-                                                   "\nImage path is: " + os.path.join(self.root_path,
-                                                                                      self.image_path))
+        return "Build Prop directory is: " + os.path.join(self.root_path, self.image_path)
 
-    def readinput(self):
+    def readinput(self, propfile):
         """read build prop file"""
-        global verbose, debug, test
-        bp_file = os.path.join(self.root_path, self.image_path, self.buildprop_file)
+        bp_file = os.path.join(self.root_path, self.image_path, propfile)
         bp_fh = open(bp_file, "r")
         dtdata = bp_fh.readlines()
         bp_fh.close()
-        if debug:
-            print(DEBUG + "Got build props file")
+        if Flags.debug:
+            print(DEBUG, "Got build props file")
         return dtdata
 
     def writeoutput(self, idout: dict):
         """write the build props to the json file"""
-        global verbose, debug, test
-        if debug:
+        if Flags.debug:
             print("{}{}".format(DEBUG, idout))
         json_file = os.path.join(self.root_path, self.output_json)
         json_fh = open(json_file, "w")
         json.dump(idout, json_fh)
         json_fh.close()
+
 
 def extractgroups(match):
     """extract all of the matching groups from the regex object"""
@@ -113,32 +129,48 @@ def extractgroups(match):
     return match.groups()
 
 
+class Flags():
+    verbose = False
+    debug = False
+    test = False
+    config = None
+    configsettings = {}
 
-def processargs():
-    """process arguments and options"""
-    usagemsg = "This program reads a json file that been output from the extract program and has a list of\
-     image urls and associated data. Then the images are downloaded, unzipped, and ripped to extract the build\
-      properties and write those to a JSON file"
 
-    parser = OptionParser(usagemsg)
-    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
-                      help="Print out helpful information during processing")
-    parser.add_option("-d", "--debug", dest="debug", action="store_true", default=False,
-                      help="Print out debug messages during processing")
-    parser.add_option("-t", "--test", dest="test", action="store_true", default=False,
-                      help="Use test file instead of full file list")
-    parser.add_option("-r", "--root", dest="rootpath", default=None,
-                      help="Root path to use for files and images", metavar="ROOTPATH")
-    parser.add_option("-i", "--image", dest="imagepath", default=None,
-                      help="Path to use for the image", metavar="IMAGEPATH")
-    parser.add_option("-o", "--output", dest="outputfile", default=None,
-                      help="JSON file to write the build.props data to", metavar="OUTPUTFILE")
-    options, args = parser.parse_args()
+class MyArgs:
+    def __init__(self):
+        # Specifc to this program
 
-    # required options checks
-    if options.debug:
-        options.verbose = True
-    return options.verbose, options.debug, options.test, options.rootpath, options.imagepath, options.outputfile
+        # Usual suspects
+        self.usagemsg = "This program reads a json file that been output from the extract program and has a list of\
+         image urls and associated data. Then the images are downloaded, unzipped, and ripped to extract the build\
+          properties and write those to a JSON file"
+
+    def processargs(self):
+        """process arguments and options"""
+        parser = OptionParser(self.usagemsg)
+        parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
+                          help="Print out helpful information during processing")
+        parser.add_option("-d", "--debug", dest="debug", action="store_true", default=False,
+                          help="Print out debug messages during processing")
+        parser.add_option("-t", "--test", dest="test", action="store_true", default=False,
+                          help="Use test file instead of full file list")
+        parser.add_option("-c", "--config", dest="config", default=None,
+                          help="Configuration file (JSON)", metavar="CONFIG")
+        options, args = parser.parse_args()
+        # required options checks
+        if options.debug:
+            options.verbose = True
+        Flags.verbose = options.verbose
+        Flags.debug = options.debug
+        Flags.test = options.test
+        Flags.config = options.config
+        if Flags.config is not None:
+            json_fh = open(Flags.config, "r")
+            Flags.configsettings = json.load(json_fh)
+            json_fh.close()
+        else:
+            eb.printerror("Missing required configuration file (--config)")
 
 
 if __name__ == '__main__':
