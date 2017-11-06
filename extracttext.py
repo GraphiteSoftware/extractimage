3e0c430df1b975830a80539740a8756906ca3326
ericstevens <eric@ericstevens.ca>
2017-11-06 15:00:01 -0500

    Updated extracttest to use the config file and beter global flags

--- extracttext.py
+++ extracttext.py
@@ -9,82 +9,104 @@ import re
 
 # define global variables
 # options as globals
-verbose = False
-debug = False
-test = False
-DEBUG = '[DEBUG] '
-VERBOSE = '[STATUS] '
+DEBUG = '[DEBUG]'
+VERBOSE = '[STATUS]'
+ERROR = '[ERROR]'
 
 
 def main():
     """main processing loop"""
-    global verbose, debug, test
+    do = MyArgs()
+    do.processargs()
+    if Flags.test:
+        print(VERBOSE, "Running in Test Mode")
+    if Flags.debug:
+        print(DEBUG,
+              "Flags are:\n\tVerbose: {}\n\tDebug: {}\n\tTest: {}\n\tConfig File: {}\n\tConfig Settings: {}".format(
+                  Flags.verbose, Flags.debug, Flags.test, Flags.config, Flags.configsettings))
     image_dict = {}
-    verbose, debug, test, root, data, images, inputfile, outputfile = processargs()
-    rw = ReadWrite(root, data, images, inputfile, outputfile)
-    for line in rw.readinput():
+    inf = ReadJson(Flags.configsettings['root'], Flags.configsettings['data'], Flags.configsettings['links'])
+    outf = WriteJson(Flags.configsettings['root'], Flags.configsettings['data'], Flags.configsettings['output'])
+    inf.readinput()
+    for line in inf.data:
         image_dict[line['name']] = processline(line['pid'], line['name'])
-    rw.writeoutput(image_dict)
+    outf.data = image_dict
+    outf.writeoutput()
 
 
-class ReadWrite:
-    def __init__(self, rootpath: str, datapath: str, imagepath: str, infile: str, outfile: str):
+class ReadJson:
+    def __init__(self, rootpath: str, datapath: str, infile: str):
         if rootpath is None:
-            self.root_path = '/Volumes/passport'
+            self.root_path = '.'
         else:
             self.root_path = rootpath
-        if imagepath is None:
-            self.image_path = 'xiaomi_images'
-        else:
-            self.image_path = imagepath
-        if datapath is None:
-            self.url_list_path = 'xiaomi_data'
-        else:
-            self.url_list_path = datapath
         if infile is None:
-            self.input_json = 'xmlist.json'
+            self.json = 'linklist.json'
         else:
-            self.input_json = infile
-        if outfile is None:
-            self.output_json = 'urllist.json'
+            self.json = infile
+        if datapath is None:
+            self.data_path = '.'
         else:
-            self.output_json = outfile
+            self.data_path = datapath
+
+        self.data = {}
 
     def __str__(self) -> str:
-        return "Input file is: " + os.path.join(self.root_path,
-                                                self.url_list_path,
-                                                self.output_json) + \
-               "\nOutput file is: " + os.path.join(self.root_path,
-                                                   self.url_list_path,
-                                                   self.output_json)
+        return "Input file is: " + os.path.join(self.root_path, self.data_path, self.json, )
 
     def readinput(self):
-        """read model and download url from json file"""
-        global verbose, debug, test
-        json_file = os.path.join(self.root_path, self.url_list_path, self.input_json)
-        json_fh = open(json_file, "r")
-        data = json.load(json_fh)
+        """read a file"""
+        json_file = os.path.join(self.root_path, self.data_path, self.json)
+        try:
+            json_fh = open(json_file, "r")
+        except IOError:
+            print(ERROR, "Failed to open input file", self.json)
+            sys.exit(1)
+        self.data = json.load(json_fh)
         json_fh.close()
-        if debug:
-            print(DEBUG + "Got json file")
-        return data
+        if Flags.debug:
+            print(DEBUG, "Got json input file", json_file)
 
-    def writeoutput(self, idout: dict):
-        """write the image list dictionary to the json file"""
-        global verbose, debug, test
-        if debug:
-            print("{}{}".format(DEBUG, idout))
-        json_file = os.path.join(self.root_path, self.url_list_path, self.output_json)
-        json_fh = open(json_file, "w")
-        json.dump(idout, json_fh)
+
+class WriteJson:
+    def __init__(self, rootpath: str, datapath: str, outfile: str):
+        if rootpath is None:
+            self.root_path = '.'
+        else:
+            self.root_path = rootpath
+        if outfile is None:
+            self.json = 'imagelist.json'
+        else:
+            self.json = outfile
+        if datapath is None:
+            self.data_path = '.'
+        else:
+            self.data_path = datapath
+
+        self.data = {}
+
+    def __str__(self) -> str:
+        return "Output file is: " + os.path.join(self.root_path, self.data_path, self.json, )
+
+    def writeoutput(self):
+        """write the build props to the json file"""
+        if Flags.debug:
+            print(DEBUG, self.data)
+        json_file = os.path.join(self.root_path, self.data_path, self.json)
+        try:
+            json_fh = open(json_file, "w")
+        except IOError:
+            print(ERROR, "Failed to open output file", self.json)
+            sys.exit(1)
+        json.dump(self.data, json_fh)
         json_fh.close()
 
-
-global_id = ''
-china_id = ''
-processing = ''
-model_name = ''
-channel = ''
+class Global:
+    global_id = ''
+    china_id = ''
+    processing = ''
+    model_name = ''
+    channel = ''
 
 
 def extractgroup(match):
@@ -95,39 +117,37 @@ def extractgroup(match):
 
 
 def walker(soup, ld):
-    global global_id, china_id, processing, model_name, channel
-    global verbose, debug, test
     if soup.name is not None:
         for child in soup.children:
             if child.name == 'span':
                 # print("<SPAN>: ", child.text, child.attrs)
                 if bool(re.search("Global$", child.text)):
-                    global_id = 'content_' + child.get('id')
-                    if debug:
+                    Global.global_id = 'content_' + child.get('id')
+                    if Flags.debug:
                         print("Found Global: " + child.text + " with id of: " + child.get('id'))
                 if bool(re.search("China", child.text)):
-                    china_id = 'content_' + child.get('id')
-                    if debug:
+                    Global.china_id = 'content_' + child.get('id')
+                    if Flags.debug:
                         print("Found China: " + child.text + " with id of: " + child.get('id'))
 
             if child.name == 'div':
                 # print("<DIV>: ", child.text, child.attrs)
                 if 'id' in child.attrs:
-                    if child.get('id') == global_id:
-                        processing = "global"
-                    if child.get('id') == china_id:
-                        processing = "china"
+                    if child.get('id') == Global.global_id:
+                        Global.processing = "global"
+                    if child.get('id') == Global.china_id:
+                        Global.processing = "china"
                 if 'class' in child.attrs:
                     for c in child.get('class'):
                         if c == 'download_nv':
-                            channel = extractgroup(re.search(r"^(.*?) ", child.text))
+                            Global.channel = extractgroup(re.search(r"^(.*?) ", child.text))
             if child.name == 'a':
                if 'class' in child.attrs:
                     # print("Found a class of:", child.get('class'))
                     if 'btn_5' in child.get('class'):
-                        if verbose:
-                            print("Processing", channel, processing, model_name, "link of:", child.get('href'))
-                            temp_d = {'image': child.get('href'), 'channel': channel, 'region': processing}
+                        if Flags.verbose:
+                            print("Processing", Global.channel, Global.processing, Global.model_name, "link of:", child.get('href'))
+                            temp_d = {'image': child.get('href'), 'channel': Global.channel, 'region': Global.processing}
                             ld['images'].append(temp_d)
             ld = walker(child, ld)
     return ld
@@ -136,10 +156,9 @@ def walker(soup, ld):
 
 def processline(pid: str, n: str):
     """process the line by reading the html and extracting the information"""
-    global verbose, debug, test
     url = "http://en.miui.com/download-" + pid + ".html"
-    if verbose:
-        print(VERBOSE + "Name: {} -> {}".format(n, url))
+    if Flags.verbose:
+        print(VERBOSE, "Name: {} -> {}".format(n, url))
     line_dict = dict(url=url, name=n, images=[])
 
     http = httplib2.Http()
@@ -148,48 +167,54 @@ def processline(pid: str, n: str):
     soup = BeautifulSoup(response, 'html.parser')
 
     line_dict = walker(soup, line_dict)
-    if debug:
+    if Flags.debug:
         print(line_dict)
     return line_dict
 
 
-def processargs():
-    """process arguments and options"""
-    usagemsg = "This program reads a json file that has the Xiaomi model list as pulled from the Xiaomi download site\n\
-     and extracts all of the image URLs from all of the download pages. The URLS are written to a json file. \n\
-    Usage is:\n\n\
-    python3 " + sys.argv[0] + " [options] where:"
+class Flags:
+    verbose = False
+    debug = False
+    test = False
+    config = None
+    configsettings = {}
 
-    parser = OptionParser(usagemsg)
-    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
-                      help="Print out helpful information during processing")
-    parser.add_option("-d", "--debug", dest="debug", action="store_true", default=False,
-                      help="Print out debug messages during processing")
-    parser.add_option("-t", "--test", dest="test", action="store_true", default=False,
-                      help="Use test file instead of full file list")
-    parser.add_option("-r", "--root", dest="rootpath", default=None,
-                      help="Root path to use for files and images", metavar="ROOTPATH")
-    parser.add_option("-a", "--data", dest="datapath", default=None,
-                      help="Path to use for files", metavar="DATAPATH")
-    parser.add_option("-i", "--images", dest="imagepath", default=None,
-                      help="Path to use for images", metavar="IMAGEPATH")
-    parser.add_option("-m", "--model", dest="modelfile", default=None,
-                      help="JSON file with model and URL data", metavar="MODELFILE")
-    parser.add_option("-o", "--output", dest="outputfile", default=None,
-                      help="JSON file to write the download URL data to", metavar="OUTPUTFILE")
-    (options, args) = parser.parse_args()
 
-    # required options checks
-    if options.debug:
-        options.verbose = True
-    return options.verbose, \
-           options.debug, \
-           options.test, \
-           options.rootpath, \
-           options.datapath, \
-           options.imagepath, \
-           options.modelfile, \
-           options.outputfile
+class MyArgs:
+    def __init__(self):
+        # Specifc to this program
+
+        # Usual suspects
+        self.usagemsg = "This program reads a json file that has the Xiaomi model list as pulled from the Xiaomi" +\
+        "download site and extracts all of the image URLs from all of the download pages. " +\
+        "The URLS are written to a json file."
+
+    def processargs(self):
+        """process arguments and options"""
+        parser = OptionParser(self.usagemsg)
+        parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
+                          help="Print out helpful information during processing")
+        parser.add_option("-d", "--debug", dest="debug", action="store_true", default=False,
+                          help="Print out debug messages during processing")
+        parser.add_option("-t", "--test", dest="test", action="store_true", default=False,
+                          help="Use test file instead of full file list")
+        parser.add_option("-c", "--config", dest="config", default=None,
+                          help="Configuration file (JSON)", metavar="CONFIG")
+
+        options, args = parser.parse_args()
+        # required options checks
+        if options.debug:
+            options.verbose = True
+        Flags.verbose = options.verbose
+        Flags.debug = options.debug
+        Flags.test = options.test
+        Flags.config = options.config
+        if Flags.config is not None:
+            json_fh = open(Flags.config, "r")
+            Flags.configsettings = json.load(json_fh)
+            json_fh.close()
+        else:
+            eb.printerror("Missing required configuration file (--config)")
 
 
 if __name__ == '__main__':
